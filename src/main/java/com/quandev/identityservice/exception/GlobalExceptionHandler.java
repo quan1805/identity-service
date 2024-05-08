1e1746462528e606d96bf3820420855dc710bb3b
Quan <quan.di1207@gmail.com>
2024-05-08 14:49:55 +0700

    handle exception and return Http Code customize

--- src/main/java/com/quandev/identityservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/quandev/identityservice/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.quandev.identityservice.exception;
 
 import com.quandev.identityservice.dto.request.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,7 +27,22 @@ public class GlobalExceptionHandler {
         apiResponse.setCode(errorCode.getCode());
         apiResponse.setMessage(errorCode.getMessage());
 
-        return ResponseEntity.badRequest().body(apiResponse);
+        return ResponseEntity
+                .status(errorCode.getStatusCode())
+                .body(apiResponse);
+    }
+
+    @ExceptionHandler(value = AccessDeniedException.class)
+    ResponseEntity<ApiResponse> handlingAccessDeniedException(AccessDeniedException exception) {
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+
+        return ResponseEntity.status(errorCode.getStatusCode())
+                .body(
+                        ApiResponse.builder()
+                                .code(errorCode.getCode())
+                                .message(errorCode.getMessage())
+                                .build()
+                );
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
